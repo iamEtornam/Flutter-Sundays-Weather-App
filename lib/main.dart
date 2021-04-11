8425860439aeb9d8656b0dcd147c6b583908cf6b
Etornam <sunumacbright@gmail.com>
2021-04-11 20:10:05 +0000

    add assets, packages, completed layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -32,24 +33,70 @@ class HomePage extends StatelessWidget {
           color: Colors.black,
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              'Cold',
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.w600),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Center(
+                  child: Text(
+                    'Cold',
+                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.w600),
+                  ),
+                ),
+                Text(
+                  'Accra, Ghana',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey),
+                ),
+              ],
             ),
-          ),
-          Text(
-            'Accra, Ghana',
-            style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-                color: Colors.grey),
-          )
-        ],
+            Column(
+              children: [
+                Image(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  fit: BoxFit.fill,
+                  image: Svg('assets/cloud.svg'),
+                ),
+                Text('19°',
+                    style: TextStyle(color: Colors.black, fontSize: 21)),
+              ],
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Next day',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600, color: Colors.black),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Image(
+                  image: Svg('assets/sun.svg'),
+                  width: 25,
+                  height: 25,
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  '22°',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.w600),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
